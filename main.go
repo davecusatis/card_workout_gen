fe6d4d19f1a50470e3728ee9ab372eb591928b6d
davecusatis <davecusatis1@gmail.com>
2020-05-08 09:39:01 -0700

    fix pkg

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
-	"strings"
 	"time"
-
-	"github.com/davecusatis/card_workout_gen/cardset"
 )
 
 const workoutLength = 20
@@ -14,14 +10,14 @@ const workoutLength = 20
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	cardSet := cardset.InitSet()
-	var randomCard string
-	var randIndex int
-	var output strings.Builder
-	for i := 0; i < workoutLength; i++ {
-		randIndex = rand.Intn(len(cardSet.Cards))
-		randomCard = cardSet.Cards[randIndex]
-		output.WriteString(fmt.Sprintf("%d. %s \n", i+1, cardSet.getCardTranslation(randomCard)))
-	}
-	fmt.Println(output.String())
+	// cardSet := cardset.InitSet()
+	// var randomCard string
+	// var randIndex int
+	// var output strings.Builder
+	// for i := 0; i < workoutLength; i++ {
+	// 	randIndex = rand.Intn(len(cardSet.Cards))
+	// 	randomCard = cardSet.Cards[randIndex]
+	// 	output.WriteString(fmt.Sprintf("%d. %s \n", i+1, cardSet.getCardTranslation(randomCard)))
+	// }
+	// fmt.Println(output.String())
 }
